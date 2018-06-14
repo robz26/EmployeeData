791a82d320953fe38affef0a52f933179dcfdd8f
Robin Zhao <37418663+robz26@users.noreply.github.com>
2018-06-13 20:07:43 -0500

    some formatting changes

--- javascript/main.js
+++ javascript/main.js
@@ -6,62 +6,62 @@ var config = {
     projectId: "time-sheet-8bf7c",
     storageBucket: "",
     messagingSenderId: "655891623227"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  // on click event listener for adding Trains
-  $("#submit").on("click", function(event) {
+// on click event listener for adding Trains
+$("#submit").on("click", function (event) {
     event.preventDefault();
 
-      // create variables to store and grab user input
-      var name = $("#name").val().trim();
-      var role = $("#role").val().trim();
-      var date = $("#date").val().trim();
-      var months = $("#months").val().trim();
-      var start = $("#start").val().trim();
-      var rate = $("#rate").val().trim();
+    // create variables to store and grab user input
+    var name = $("#name").val().trim();
+    var role = $("#role").val().trim();
+    var date = $("#date").val().trim();
+    var months = $("#months").val().trim();
+    var start = $("#start").val().trim();
+    var rate = $("#rate").val().trim();
 
-        
-        // add data to firebase
-      database.ref().push({
+
+    // add data to firebase
+    database.ref().push({
         name: name,
         role: role,
         date: date,
         months: months,
         start: start,
         rate: rate
-        });
+    });
 
-   // Alert
+    // Alert
     alert("Employee added succefully!");
 
     // clear all of the text-boxes for new submission
-      $("#name").val("");
-      $("#role").val("");
-      $("#date").val("");
-      $("#months").val("");
-      $("#start").val("");
-      $("#rate").val("");
-  
-  });
+    $("#name").val("");
+    $("#role").val("");
+    $("#date").val("");
+    $("#months").val("");
+    $("#start").val("");
+    $("#rate").val("");
 
-  // create Firebase event for adding to the database and a row in the html when a user adds an entry
-	database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+});
 
-	  console.log(childSnapshot.val());
+// create Firebase event for adding to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
-	  // get the values and store them into a variable.
-	  var name = childSnapshot.val().name;
-	  var role = childSnapshot.val().role;
-	  var date = childSnapshot.val().date;
-	  var months = childSnapshot.val().months;
-	  var start = childSnapshot.val().start;
-    var rate = childSnapshot.val(). rate;
-    
+    console.log(childSnapshot.val());
+
+    // get the values and store them into a variable.
+    var name = childSnapshot.val().name;
+    var role = childSnapshot.val().role;
+    var date = childSnapshot.val().date;
+    var months = childSnapshot.val().months;
+    var start = childSnapshot.val().start;
+    var rate = childSnapshot.val().rate;
+
     // Add each data into the table
-	  $("#table > tbody").append("<tr><td>" + name + "</td><td>" +  + "</td><td>" + role + 
-    "</td><td>" + date + "</td><td>" + months + "</td><td>" + start + "</td><td>" + rate + "</td></tr>");
+    $("#table > tbody").append("<tr><td>" + name + "</td><td>" + + "</td><td>" + role +
+        "</td><td>" + date + "</td><td>" + months + "</td><td>" + start + "</td><td>" + rate + "</td></tr>");
 
 });
