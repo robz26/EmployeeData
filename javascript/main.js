49d3bacb64350c5fd99043249e6fbb3d60b3b2de
Melinh Mai <melinh.mai@gmail.com>
2018-06-13 19:14:43 -0500

    adding firebase

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAKhtdAL06SFSz2tgxoJHszhKl1-jLI3TU",
+    authDomain: "time-sheet-8bf7c.firebaseapp.com",
+    databaseURL: "https://time-sheet-8bf7c.firebaseio.com",
+    projectId: "time-sheet-8bf7c",
+    storageBucket: "",
+    messagingSenderId: "655891623227"
+  };
+  firebase.initializeApp(config);
