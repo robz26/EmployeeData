708393ce8864f177eba361ed8dd70d9acbdea7bc
Med Wasfy <medwasfy@gmail.com>
2018-06-13 19:54:12 -0500

    js update

--- javascript/main.js
+++ javascript/main.js
@@ -11,14 +11,53 @@ var config = {
 
   var database = firebase.database();
 
-  ("body").on("click", "#submit", function(){
-      var name = $("#name").val().trim()
-      var role = $("#role").val().trim()
-      var date = $("#date").val().trim()
-      var name = $("#months").val().trim()
-      var name = $("#start").val().trim()
-      var name = $("#rate").val().trim()
+  // on click event listener for adding Trains
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
 
-      var newRow;
-      var 
-  })
+      // create variables to store and grab user input
+      var name = $("#name").val().trim();
+      var role = $("#role").val().trim();
+      var date = $("#date").val().trim();
+      var name = $("#months").val().trim();
+      var name = $("#start").val().trim();
+      var name = $("#rate").val().trim();
+
+        
+        // add data to firebase
+      database.ref().push({
+        name: name,
+        role: role,
+        date: date,
+        months: months,
+        start: start,
+        rate: rate
+        });
+
+   // Alert
+    alert("Employee added succefully!");
+
+    // clear all of the text-boxes for new submission
+      $("#name").val("");
+      $("#role").val("");
+      $("#date").val("");
+      $("#months").val("");
+      $("#start").val("");
+      $("#rate").val("");
+  
+  });
+
+  // create Firebase event for adding train to the database and a row in the html when a user adds an entry
+	database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+	  console.log(childSnapshot.val());
+
+	  // get the values and store them into a variable.
+	  var name = childSnapshot.val().name;
+	  var role = childSnapshot.val().role;
+	  var date = childSnapshot.val().date;
+	  var months = childSnapshot.val().months;
+	  var start = childSnapshot.val().start;
+	  var rate = childSnapshot.val(). rate;
+
+});
